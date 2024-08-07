a6df85a0059d05fd7786e07a4ee63fac5d783576
Dinesh-aragonda-79 <106001627+Dinesh-aragonda-79@users.noreply.github.com>
2024-08-07 10:45:01 +0530

    Update SignupPage.js

--- Frontend/src/pages/SignupPage.js
+++ Frontend/src/pages/SignupPage.js
@@ -136,7 +136,7 @@ const SignupPage = () => {
     e.preventDefault();
 
     try {
-      const response = await axios.post('http://localhost:5000/api/auth/register', formData);
+      const response = await axios.post('https://code-tracker-1.onrender.com/api/auth/register', formData);
       const { token, username } = response.data;
 
       login(token, username);
